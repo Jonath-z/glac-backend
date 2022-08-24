af91838cf2a4cda221d45c652c980eaa259b99e9
Jonath-z <jonathanzihindula95@gmail.com>
2022-08-24 14:00:05 +0200

    res sent

--- src/middleware/getData/index.ts
+++ src/middleware/getData/index.ts
@@ -2,6 +2,13 @@ import { NextFunction, Request, Response } from "express";
 
 const getData = async (req: Request, res: Response, next: NextFunction) => {
   console.log("comming request", req);
+
+  res.send().json({
+    message: "request received",
+    request: req,
+  });
+
+  next();
 };
 
 export default getData;
