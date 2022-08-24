8a2d97a391147d7159d5564e7c65a1845380ca63
Jonath-z <jonathanzihindula95@gmail.com>
2022-08-24 13:42:34 +0200

    build file removed

--- build/routes/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var geDataRoute_1 = require("./geDataRoute");
-var routes = [geDataRoute_1.getDataRoute];
-exports.default = routes;
